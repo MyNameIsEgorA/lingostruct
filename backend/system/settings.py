d6c0b56a393aae620322fc7c3323ccd627a39c34
Ophely-git <ophely1992@gmail.com>
2024-05-19 17:25:00 +0300

    create profile app

--- backend/system/settings.py
+++ backend/system/settings.py
@@ -16,6 +16,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+
+    'profile_user',
 ]
 
 MIDDLEWARE = [
--- backend/system/urls.py
+++ backend/system/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/profile/', include('profile_user.urls')),
 ]
 
 if settings.DEBUG:
