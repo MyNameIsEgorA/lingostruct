eef06daae0873095ca3a1b69c05513c69006c29d
Ophely-git <Ophely-git@gmail.com>
2024-05-25 19:44:59 +0300

    resolve staticfilesdirs

--- backend/system/settings.py
+++ backend/system/settings.py
@@ -117,9 +117,9 @@ TIME_ZONE = 'Europe/Moscow'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static/')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'images')]
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
