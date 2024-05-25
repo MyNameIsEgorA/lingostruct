8db6c31a1951c2a22e663066b63bf7a08d336b60
Ophely-git <Ophely-git@gmail.com>
2024-05-25 19:40:38 +0300

    deploy

--- backend/system/settings.py
+++ backend/system/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = 'django-insecure-4d8otobg*_cs=-t71uv2oh!q!g9q9v7ojd%b)flmx_a@u$og-k
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['5.35.4.98']
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -118,6 +118,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static/')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
 
 MEDIA_URL = 'media/'
